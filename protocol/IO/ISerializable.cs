31113d91ef88c5d77b0a2548b9d0ad13ef4ba26e
MrXyfir <mr@xyfir.com>
2018-03-24 20:39:50 -0700

    Comments and translations

--- protocol/IO/ISerializable.cs
+++ protocol/IO/ISerializable.cs
@@ -3,22 +3,14 @@
 namespace Neo.IO
 {
     /// <summary>
-    /// 为序列化提供一个接口
+    /// An interface for serialization
     /// </summary>
     public interface ISerializable
     {
         int Size { get; }
 
-        /// <summary>
-        /// 序列化
-        /// </summary>
-        /// <param name="writer">存放序列化后的结果</param>
         void Serialize(BinaryWriter writer);
 
-        /// <summary>
-        /// 反序列化
-        /// </summary>
-        /// <param name="reader">数据来源</param>
         void Deserialize(BinaryReader reader);
     }
 }
