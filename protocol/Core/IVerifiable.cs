f0edf8e9b0c7a35c6b2a4f49554c2526bd2727ba
MrXyfir <mr@xyfir.com>
2018-03-24 20:39:18 -0700

    Comments and translations

--- protocol/Core/IVerifiable.cs
+++ protocol/Core/IVerifiable.cs
@@ -5,31 +5,28 @@ using System.IO;
 namespace Neo.Core
 {
     /// <summary>
-    /// 为需要签名的数据提供一个接口
+    /// An interface for data that needs to be signed
     /// </summary>
     public interface IVerifiable : ISerializable, IScriptContainer
     {
         /// <summary>
-        /// 用于验证该对象的脚本列表
+        /// Scripts used to validate the object
         /// </summary>
         Witness[] Scripts { get; set; }
-        
+
         /// <summary>
-        /// 反序列化未签名的数据
+        /// Deserialize unsigned data
         /// </summary>
-        /// <param name="reader">数据来源</param>
         void DeserializeUnsigned(BinaryReader reader);
 
         /// <summary>
-        /// 获得需要校验的脚本Hash值
+        /// Return script hashes that need verification
         /// </summary>
-        /// <returns>返回需要校验的脚本Hash值</returns>
         UInt160[] GetScriptHashesForVerifying();
-        
+
         /// <summary>
-        /// 序列化未签名的数据
+        /// Serialize unsigned data
         /// </summary>
-        /// <param name="writer">存放序列化后的结果</param>
         void SerializeUnsigned(BinaryWriter writer);
     }
 }
